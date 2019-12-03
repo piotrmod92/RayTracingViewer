d0e2ec4b967db252a8259be926ff5cef7b5a0eb9
Piotr Modzelewski <piotrmod92@gmail.com>
2019-12-03 22:59:08 +0100

    Add user interface and adjust layout
     - styled labels, inputs, and button added,
     - layout modified (font, colors, shadows, ...),
     - initial onclick listener for button added.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-(function() {
-    console.log('ready');
-})()
+(() => {
+    // Variables definition
+    let angle = 0;
+
+    // Functions definitions
+    const addScanButtonOnclick = () => {
+        const $button = document.getElementsByClassName('angle-scan')[0];
+        $button.addEventListener('click', () => {
+            if ($button.classList.contains('angle-scan--start')) {
+                $button.classList.remove('angle-scan--start');
+                $button.classList.add('angle-scan--pause');
+                $button.innerHTML = 'Pause angle scan';
+            } else if ($button.classList.contains('angle-scan--pause')) {
+                $button.classList.remove('angle-scan--pause');
+                $button.classList.add('angle-scan--resume');
+                $button.innerHTML = 'Resume angle scan';
+            } else if ($button.classList.contains('angle-scan--resume')) {
+                $button.classList.remove('angle-scan--resume');
+                $button.classList.add('angle-scan--pause');
+                $button.innerHTML = 'Pause angle scan';
+            }
+        });
+    }
+
+    // Run application
+    addScanButtonOnclick();
+
+})();
